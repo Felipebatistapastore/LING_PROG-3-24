c4d01373bffe106027c97a097970f222af3ba157
Felipebatistapastore <125515160+Felipebatistapastore@users.noreply.github.com>
2023-03-01 20:49:42 -0300

    Add files via upload

---
+++ LING_PROG-3-23-main/LING_PROG-3-23-main/1_intro/6_if_else.php
@@ -0,0 +1,27 @@
+<?php
+  
+  $idade = 19;
+  
+  if($idade <= 18) {
+	  echo 'vocè não pode entrar aqui! <HR> ';
+  } else {
+	  echo 'Seja Bem - vindo <HR> ';
+  }
+  
+   $idade = 19;
+  
+  if($idade < 18) {
+	  echo 'vocè não pode entrar aqui <hr> !';
+  } else {
+	  echo 'Seja Bem - vindo <hr> ';
+  }
+  
+   $idade = 15;
+  
+  if($idade <= 20) {
+	  echo 'vocè não pode entrar aqui! <HR> ';
+  } else {
+	  echo 'Seja Bem - vindo <HR> ';
+  }
+  
+?>
